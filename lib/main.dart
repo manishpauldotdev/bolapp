6e414985b98ab10bc06cd2ec941097dd9b23e631
Manish Paul <manishpaul.dev@gmail.com>
2025-01-19 23:38:49 +0530

    added riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,31 @@
+import 'package:bolapp/core/providers/theme_notifier.dart';
 import 'package:bolapp/core/styles/app_theme.dart';
 import 'package:bolapp/features/splash/splash_page.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MainApp());
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  runApp(
+    const ProviderScope(
+      child: MainApp(),
+    ),
+  );
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends ConsumerWidget {
   const MainApp({super.key});
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final themeState = ref.watch(themeNotifierProvider);
+
     return MaterialApp(
       theme: AppTheme.lightTheme,
       darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
+      themeMode: themeState == ThemeMode.light ? ThemeMode.dark : ThemeMode.light,
       home: SplashPage(),
     );
   }
